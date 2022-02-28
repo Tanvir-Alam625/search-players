955970041f2342f736d68a4d601bdb64d9c54245
tanveer <mdtanviralamht731@gmail.com>
2022-02-28 22:54:21 +0600

    delete button working

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const searchPlayers = async ()=>{
     if(emptyV == ""){
         input.style.border = '2px solid red';
     }else{
-    input.style.border = '2px solid green'
+    input.style.border = '2px solid gray'
     document.getElementById('loader-container').style.display = 'block';
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${input.value}`;
     const response = await fetch(url);
@@ -23,21 +23,21 @@ const getLoadPlayer = (players) =>{
     try{
         playerError.innerText = "";
         container.innerHTML = "";
-    players.forEach(player => {
-        const div = document.createElement('div');
-        div.className = 'p-2 m-4 rounded border';
-        div.innerHTML = `
-                    <div class="img-div  mb-4 ">
-                        <img src="${player.strThumb}" width="100%" class="rounded" alt="player">
-                    </div>
-                    <h3 class="text-2xl mb-2">${player.strPlayer}</h3>
-                    <h4 class="text-1xl">${player.strNationality} </h4>
-                    <div class="player-btn flex justify-between my-3">
-                        <button id="delete-btn" class=" delete-button py-2 px-4 rounded border bg-red-500 text-white">Delete</button>
-                        <button onclick="detailsPlayer('${player.idPlayer}')" class="py-2 px-4 rounded border bg-green-500 text-white">Details</button>
-                    </div>
-        `;
-        container.appendChild(div);
+        players.forEach(player => {
+            const div = document.createElement('div');
+            div.className = 'p-2 m-4 rounded border';
+            div.innerHTML = `
+                        <div class="img-div  mb-4 ">
+                            <img src="${player.strThumb}" width="100%" class="rounded" alt="player">
+                        </div>
+                        <h3 class="text-2xl mb-2">${player.strPlayer}</h3>
+                        <h4 class="text-1xl">${player.strNationality} </h4>
+                        <div class="player-btn flex justify-between my-3">
+                            <button id="delete-btn" class=" delete-button py-2 px-4 rounded border bg-red-500 text-white">Delete</button>
+                            <button onclick="detailsPlayer('${player.idPlayer}')" class="py-2 px-4 rounded border bg-green-500 text-white">Details</button>
+                        </div>
+            `;
+            container.appendChild(div);
     });
     }catch(error){
         playerError.innerText = "No Players Items";
@@ -50,7 +50,6 @@ const getLoadPlayer = (players) =>{
         });
     }
 }
-
 // details api data load function 
 const detailsPlayer = async (player) =>{
     const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${player}`;
